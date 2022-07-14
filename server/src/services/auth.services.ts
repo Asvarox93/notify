0b9ca6cf39a4f72b7ae476cfbf69af0eff429173
Sebastian Białek <asvarox93@gmail.com>
2022-07-14 20:04:36 +0200

    Fix error type in jwt verify function

--- server/src/services/auth.services.ts
+++ server/src/services/auth.services.ts
@@ -1,5 +1,6 @@
 import  jwt  from 'jsonwebtoken';
 import { NextFunction,Request, Response } from "express";
+import { setErrorMessage } from '../util/util';
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization']
@@ -8,9 +9,9 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
   if (token == null) return res.status(401).send({status:401, message:"Unable to authenticate"})
 
   
-  jwt.verify(token, process.env.TOKEN_SECRET, (err: any)  => { 
-    if (err) return res.status(403).send({ status: 403, message: err.message })
-     console.log("Tu jestem2")
+  jwt.verify(token, process.env.TOKEN_SECRET, (error: unknown) => { 
+    const message = setErrorMessage(error)
+    if (error) return res.status(403).send({ status: 403, message: message })
     next()
   })
 }
